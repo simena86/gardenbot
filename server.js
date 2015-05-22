9ab8cbffe4f9d892a9c815bce180d7b4cf60eddc
simena86 <simena86@gmail.com>
2015-05-22 10:11:54 +0200

    fc

---
+++ server.js
@@ -0,0 +1,136 @@
+
+function onLogin(){
+	console.log('started server on port 3000');
+}
+
+// express
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.get('/mongo_data.json',function(request,response){
+	console.log('get data');
+	response.send( JSON.stringify( SENSOR_DATA) );
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(__dirname + '/public'));
+
+
+// start server
+var server = app.listen(3000, onLogin());
+var io = require('socket.io').listen(server);
+
+// on socket.io connection
+io.sockets.on('connection', function (socket) {
+	socket.on('cmd1', function (data) {
+		handleCmd1(data);
+	});
+	socket.on('cmd2', function (data) {
+		handleCmd2(data);
+	});
+});
+
+function handleCmd1(data){
+	console.log('handling cmd1 with msg:');
+	console.log(data);
+}
+
+function handleCmd2(data){
+	console.log('handling cmd2 with msg:');
+	console.log(data);
+}
+
+// update every 4 minutes
+var counter1 = 0;
+var interval = 3 * 1000;
+var dayCntr = 0;
+setInterval( onSensorReading, interval);
+
+function getNewSensorData(){
+	var r = Math.random();
+	var r1 = Math.random();
+	var msg = {message: r};
+	dayCntr ++;
+  var aDate1 = new Date();
+  var aDate2 = new Date();
+	counter1++;
+	var newDataObj = {
+		sensors : {
+			power : [aDate1, r, null],
+			temp : [aDate2, r+3, null]		
+		}
+	}
+	return newDataObj;
+}
+
+// send new data as JSON object
+// data is sent over socket.io
+function sendData2Client(newData){
+	io.emit('sensor_input',newData);
+}
+
+function onSensorReading(){
+	var newData = getNewSensorData();
+	sendData2Client( newData );
+	updateDatabase( newData );
+}
+
+function updateDatabase(newData){
+
+}
+
+var SENSOR_DATA = {
+	raw1 : [[2015, 04, 17, 0, 00, 34, 01], 
+				 [2015, 05, 17, 0, 01, 35, 02], 
+				 [2015, 05, 17, 0, 02, 35, 02], 
+				 [2015, 05, 17, 0, 03, 35, 02], 
+				 [2015, 05, 18, 0, 04, 35, 09]] ,
+
+	raw2 : [[2015, 04, 17, 0, 09, 34, 05], 
+				 [2015, 05, 17, 0, 05, 35, 06], 
+				 [2015, 05, 17, 0, 06, 35, 06], 
+				 [2015, 05, 17, 0, 07, 35, 06], 
+				 [2015, 05, 18, 0, 06, 35, 02]],
+}
+
+
+/*
+//var db_url = "mongodb://simen2:raspberry@ds029640.mongolab.com:29640/home_automation";
+var db_url = "mongodb://simen2:raspberry@ds029640.mongolab.com:29640/home_automation";
+var collections = ["test_collection"]
+console.log('done 0');
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+
+//var db = require("mongojs").connect(databaseUrl, collections);
+
+MongoClient.connect(db_url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    //HURRAY!! We are connected. :)
+    console.log('Connection established to', url);
+
+    // Get the documents collection
+    var collection = db.collection('test_collection');
+
+    //Create some users
+    var user1 = {name: 'modulus admin', age: 42, roles: ['admin', 'moderator', 'user']};
+    var user2 = {name: 'modulus user', age: 22, roles: ['user']};
+    var user3 = {name: 'modulus super admin', age: 92, roles: ['super-admin', 'admin', 'moderator', 'user']};
+
+    // Insert some users
+    collection.insert([user1, user2, user3], function (err, result) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Inserted %d documents into the "users" collection. The documents inserted with "_id" are:', result.length, result);
+      }
+      //Close connection
+      db.close();
+    });
+  }
+});
+
+*/
